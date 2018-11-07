a343f40224473a28016bd5b06be23a74d2e4f5f2
GodotP <GodotP@5b002e9e-db92-5c4b-be5d-5bcaed4e342c>
2018-11-07 14:18:21 +0000

    upgrade version number + php-cs-fixer + re-activate Thread-Index usage  fixes #1106 @3h0m
    git-svn-id: http://ardevd20.ar.ray.group/svn/GLPI/z-others/plugins/mailanalyzer/trunk@1832 5b002e9e-db92-5c4b-be5d-5bcaed4e342c

--- setup.php
+++ setup.php
@@ -6,37 +6,36 @@ function plugin_init_mailanalyzer() {
 
    global $PLUGIN_HOOKS;
 
-   Plugin::registerClass('PluginMailAnalyzer', array('classname' => 'PluginMailAnalyzer'));
+   Plugin::registerClass('PluginMailAnalyzer', ['classname' => 'PluginMailAnalyzer']);
 
    $PLUGIN_HOOKS['csrf_compliant']['mailanalyzer']    = true;
 
+   $PLUGIN_HOOKS['pre_item_add']['mailanalyzer'] = [
+          'Ticket' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer'],
+          'TicketFollowup' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer_followup']
+      ];
 
-   $PLUGIN_HOOKS['pre_item_add']['mailanalyzer'] = array(
-      	'Ticket' => array('PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer'),
-      	'TicketFollowup' => array('PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer_followup')
-      );
-
-   $PLUGIN_HOOKS['item_add']['mailanalyzer'] = array(
-      	'Ticket' => array('PluginMailAnalyzer', 'plugin_item_add_mailanalyzer')
-      );
+   $PLUGIN_HOOKS['item_add']['mailanalyzer'] = [
+          'Ticket' => ['PluginMailAnalyzer', 'plugin_item_add_mailanalyzer']
+      ];
 
 }
 
 // Get the name and the version of the plugin - Needed
-function plugin_version_mailanalyzer(){
+function plugin_version_mailanalyzer() {
    global $LANG;
 
-   return array ('name'           => 'Mail Analyzer',
-                 'version'        => '1.3.8',
+   return  ['name'           => 'Mail Analyzer',
+                 'version'        => '1.4.0',
                  'author'         => 'Olivier Moron',
                  'license'        => 'GPLv2+',
                  'homepage'       => 'https://github.com/tomolimo/mailanalyzer',
-                 'minGlpiVersion' => '9.1');
+                 'minGlpiVersion' => '9.1'];
 }
 
 // Optional : check prerequisites before install : may print errors or add to message after redirect
-function plugin_mailanalyzer_check_prerequisites(){
-   if (version_compare(GLPI_VERSION,'9.1','lt') ) {
+function plugin_mailanalyzer_check_prerequisites() {
+   if (version_compare(GLPI_VERSION, '9.1', 'lt')) {
       echo "This plugin requires GLPI >= 9.1";
       return false;
    } else {
@@ -44,7 +43,7 @@ function plugin_mailanalyzer_check_prerequisites(){
    }
 }
 
-function plugin_mailanalyzer_check_config(){
+function plugin_mailanalyzer_check_config() {
    return true;
 }
 
