8c3c4583b3a7e341928b7968b5e5b6fe2a91bf1b
TresvauxC <TresvauxC@5b002e9e-db92-5c4b-be5d-5bcaed4e342c>
2018-12-11 13:18:58 +0000

    merge fomr 9.2  refs #1127 @0h30m
    git-svn-id: http://ardevd20.ar.ray.group/svn/GLPI/z-others/plugins/mailanalyzer/trunk@1966 5b002e9e-db92-5c4b-be5d-5bcaed4e342c

--- setup.php
+++ setup.php
@@ -1,49 +1,49 @@
 <?php
 
-
+define ("PLUGIN_MAILANALYSER_VERSION", "1.5.0");
 // Init the hooks of the plugins -Needed
 function plugin_init_mailanalyzer() {
 
-   global $PLUGIN_HOOKS;
+    global $PLUGIN_HOOKS;
 
-   Plugin::registerClass('PluginMailAnalyzer', ['classname' => 'PluginMailAnalyzer']);
+    Plugin::registerClass('PluginMailAnalyzer', ['classname' => 'PluginMailAnalyzer']);
 
-   $PLUGIN_HOOKS['csrf_compliant']['mailanalyzer']    = true;
+    $PLUGIN_HOOKS['csrf_compliant']['mailanalyzer']    = true;
 
-   $PLUGIN_HOOKS['pre_item_add']['mailanalyzer'] = [
-          'Ticket' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer'],
-          'TicketFollowup' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer_followup']
-      ];
+    $PLUGIN_HOOKS['pre_item_add']['mailanalyzer'] = [
+           'Ticket' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer'],
+           'TicketFollowup' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer_followup']
+       ];
 
-   $PLUGIN_HOOKS['item_add']['mailanalyzer'] = [
-          'Ticket' => ['PluginMailAnalyzer', 'plugin_item_add_mailanalyzer']
-      ];
+    $PLUGIN_HOOKS['item_add']['mailanalyzer'] = [
+           'Ticket' => ['PluginMailAnalyzer', 'plugin_item_add_mailanalyzer']
+       ];
 
 }
 
 // Get the name and the version of the plugin - Needed
 function plugin_version_mailanalyzer() {
-   global $LANG;
+    global $LANG;
 
-   return  ['name'           => 'Mail Analyzer',
-                 'version'        => '1.4.0',
-                 'author'         => 'Olivier Moron',
-                 'license'        => 'GPLv2+',
-                 'homepage'       => 'https://github.com/tomolimo/mailanalyzer',
-                 'minGlpiVersion' => '9.1'];
+    return  ['name'           => 'Mail Analyzer',
+                  'version'        => PLUGIN_MAILANALYSER_VERSION,
+                  'author'         => 'Olivier Moron',
+                  'license'        => 'GPLv2+',
+                  'homepage'       => 'https://github.com/tomolimo/mailanalyzer',
+                  'minGlpiVersion' => '9.1'];
 }
 
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_mailanalyzer_check_prerequisites() {
-   if (version_compare(GLPI_VERSION, '9.1', 'lt')) {
-      echo "This plugin requires GLPI >= 9.1";
-      return false;
-   } else {
-      return true;
-   }
+    if (version_compare(GLPI_VERSION, '9.1', 'lt')) {
+        echo "This plugin requires GLPI >= 9.1";
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function plugin_mailanalyzer_check_config() {
-   return true;
+    return true;
 }
 
