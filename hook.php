96c863602d90e5c60f15f2fe6a45fd65fcf25d43
MoronO <MoronO@5b002e9e-db92-5c4b-be5d-5bcaed4e342c>
2020-12-08 13:25:02 +0000

    Clean some codes: deleted all "OnBehalfOf" features to move them into a new plugin, in order to keep mailanalyzer as an email analyzer to keep track of email conversations Clarify readme.md content  refs #1689 @1h0m
    git-svn-id: http://ardevd20.ar.ray.group/svn/GLPI/z-others/plugins/mailanalyzer/branches/9.5-2.0-bugfixes@2813 5b002e9e-db92-5c4b-be5d-5bcaed4e342c

--- hook.php
+++ hook.php
@@ -47,7 +47,6 @@ function plugin_mailanalyzer_uninstall() {
 
 class PluginMailAnalyzer {
 
-
    /**
     * Create default mailgate
     * @param int $mailgate_id is the id of the mail collector in GLPI DB
@@ -83,91 +82,92 @@ class PluginMailAnalyzer {
    }
 
 
-   /**
-    * Summary of plugin_pre_item_add_mailanalyzer_followup
-    * @param mixed $parm
-    */
-   public static function plugin_pre_item_add_mailanalyzer_followup($parm) {
-       global $DB;
+   ///**
+   // * Summary of plugin_pre_item_add_mailanalyzer_followup
+   // * @param mixed $parm
+   // */
+   //public static function plugin_pre_item_add_mailanalyzer_followup($parm) {
+   //    global $DB;
 
-       if (isset($parm->input['_mailgate'])
-          && !isset($parm->input['_from_plugin_pre_item_add_mailanalyzer'])) {
-         // change requester if needed
-         $locUser = new User();
-         $str = self::getTextFromHtml($parm->input['content']);
-         $users_id = self::getUserOnBehalfOf($str);
-         if ($users_id !== false) {
-            if ($locUser->getFromDB($users_id)) {
-               // set users_id
-               $parm->input['users_id'] = $users_id;
-            }
-         }
-      }
-   }
+   //    if (isset($parm->input['_mailgate'])
+   //       && !isset($parm->input['_from_plugin_pre_item_add_mailanalyzer'])) {
+   //      // change requester if needed
+   //      $locUser = new User();
+   //      $str = self::getTextFromHtml($parm->input['content']);
+   //      $users_id = self::getUserOnBehalfOf($str);
+   //      if ($users_id !== false) {
+   //         if ($locUser->getFromDB($users_id)) {
+   //            // set users_id
+   //            $parm->input['users_id'] = $users_id;
+   //         }
+   //      }
+   //   }
+   //}
 
 
-   /**
-    * Summary of getTextFromHtml
-    * gets bare text content from HTML
-    * deltes HTML entities, but <br>
-    * @param mixed $str HTML input
-    * @return string bare text
-    */
-   public static function getTextFromHtml($str) {
-      $ret = Toolbox::unclean_html_cross_side_scripting_deep($str);
-      $ret = preg_replace("/<(p|br|div)( [^>]*)?".">/i", "\n", $ret);
-      $ret = preg_replace("/(&nbsp;| |\xC2\xA0)+/", " ", $ret);
-      $ret = strip_tags($ret);
-      $ret = html_entity_decode(html_entity_decode($ret, ENT_QUOTES));
-      return $ret;
-   }
+   ///**
+   // * Summary of getTextFromHtml
+   // * gets bare text content from HTML
+   // * deltes HTML entities, but <br>
+   // * @param mixed $str HTML input
+   // * @return string bare text
+   // */
+   //public static function getTextFromHtml($str) {
+   //   $ret = Toolbox::unclean_html_cross_side_scripting_deep($str);
+   //   $ret = preg_replace("/<(p|br|div)( [^>]*)?".">/i", "\n", $ret);
+   //   $ret = preg_replace("/(&nbsp;| |\xC2\xA0)+/", " ", $ret);
+   //   $ret = strip_tags($ret);
+   //   $ret = html_entity_decode(html_entity_decode($ret, ENT_QUOTES));
+   //   return $ret;
+   //}
 
-   /**
-    * Summary of getUserOnBehalfOf
-    * search for ##From if it exists, then try to find users_id from DB
-    * @param string $str
-    * @return boolean|integer
-    */
-   public static function getUserOnBehalfOf($str) {
-      global $DB;
 
-      // search for ##From if it exists, then try to find real requester from DB
-      $str = str_replace(['\n', '\r\n'], "\n", $str); // to be sure that \n (end of line) will not be confused with a \ in firstname
+   ///**
+   // * Summary of getUserOnBehalfOf
+   // * search for ##From if it exists, then try to find users_id from DB
+   // * @param string $str
+   // * @return boolean|integer
+   // */
+   //public static function getUserOnBehalfOf($str) {
+   //   global $DB;
 
-      $ptnUserFullName = '/##From\s*:\s*(["\']?(?\'last\'[\w.\-\\\\\' ]+)[, ]\s*(?\'first\'[\w+.\-\\\\\' ]+))?.*?(?\'email\'[\w_.+\-]+@[\w\-]+\.[\w\-.]+)?\W*$/imu';
+   //   // search for ##From if it exists, then try to find real requester from DB
+   //   $str = str_replace(['\n', '\r\n'], "\n", $str); // to be sure that \n (end of line) will not be confused with a \ in firstname
 
-      if (preg_match_all($ptnUserFullName, $str, $matches, PREG_SET_ORDER) > 0) {
-         // we found at least one ##From:
-         // then we try to get its user id from DB
-         // if an email has been found, then we try it
-         // else we try with name and firstname in this order
-         $matches = $matches[0];
-         if (isset($matches['email'])) {
-            $where2 = ['glpi_useremails.email' => $matches['email']];
-         } else {
-            $where2 = ['AND' => ['glpi_users.realname'         => $DB->escape(trim( $matches['last'] )),
-                                 'glpi_users.firstname'        => $DB->escape(trim( $matches['first'] )),
-                                 'glpi_useremails.is_default'  => 1
-                                 ]];
-         }
-         $where2['AND']['glpi_users.is_active'] = 1;
-         $where2['AND']['glpi_users.is_deleted'] = 0;
-         $res = $DB->request([
-            'SELECT'    => 'glpi_users.id',
-            'FROM'      => 'glpi_users',
-            'RIGHT JOIN'=> ['glpi_useremails' => ['FKEY' => ['glpi_useremails' => 'users_id', 'glpi_users' => 'id']]],
-            'WHERE'     => $where2,
-            'LIMIT'     => 1
-            ]);
+   //   $ptnUserFullName = '/##From\s*:\s*(["\']?(?\'last\'[\w.\-\\\\\' ]+)[, ]\s*(?\'first\'[\w+.\-\\\\\' ]+))?.*?(?\'email\'[\w_.+\-]+@[\w\-]+\.[\w\-.]+)?\W*$/imu';
 
-         if ($row = $res->next()) {
-            return (integer)$row['id'];
-         }
-      }
+   //   if (preg_match_all($ptnUserFullName, $str, $matches, PREG_SET_ORDER) > 0) {
+   //      // we found at least one ##From:
+   //      // then we try to get its user id from DB
+   //      // if an email has been found, then we try it
+   //      // else we try with name and firstname in this order
+   //      $matches = $matches[0];
+   //      if (isset($matches['email'])) {
+   //         $where2 = ['glpi_useremails.email' => $matches['email']];
+   //      } else {
+   //         $where2 = ['AND' => ['glpi_users.realname'         => $DB->escape(trim( $matches['last'] )),
+   //                              'glpi_users.firstname'        => $DB->escape(trim( $matches['first'] )),
+   //                              'glpi_useremails.is_default'  => 1
+   //                              ]];
+   //      }
+   //      $where2['AND']['glpi_users.is_active'] = 1;
+   //      $where2['AND']['glpi_users.is_deleted'] = 0;
+   //      $res = $DB->request([
+   //         'SELECT'    => 'glpi_users.id',
+   //         'FROM'      => 'glpi_users',
+   //         'RIGHT JOIN'=> ['glpi_useremails' => ['FKEY' => ['glpi_useremails' => 'users_id', 'glpi_users' => 'id']]],
+   //         'WHERE'     => $where2,
+   //         'LIMIT'     => 1
+   //         ]);
 
-      return false;
+   //      if ($row = $res->next()) {
+   //         return (integer)$row['id'];
+   //      }
+   //   }
 
-   }
+   //   return false;
+
+   //}
 
 
     /**
@@ -181,36 +181,8 @@ class PluginMailAnalyzer {
       if (isset($parm->input['_mailgate'])) {
          // this ticket have been created via email receiver.
          // and we have the Laminas\Mail\Storage\Message object in the _message key
-
-
-         // change requester if needed
-         // search for ##From if it exists, then try to find real requester from DB
-         $locUser = new User();
-         if (isset($parm->input['itemtype']) && $parm->input['itemtype'] == 'Ticket') {
-            $ticketId = (isset($parm->input['items_id']) ? $parm->input['items_id'] : $parm->fields['items_id'] );
-            $locTicket = new Ticket;
-            if ($locTicket->getFromDB( $ticketId ) && isset( $parm->input['content'] )) {
-               self::addWatchers( $locTicket, $parm->input['content'] );
-            }
-         }
-         $str = self::getTextFromHtml($parm->input['content']);
-         $users_id = self::getUserOnBehalfOf($str);
-         if ($users_id !== false) {
-            if ($locUser->getFromDB($users_id)) {
-               // set user_id and user_entity only if 'post-only' profile is found and unique
-               $entity = Profile_User::getEntitiesForProfileByUser($users_id, 1 ); // 1 if the post-only or self-service profile
-               if (count( $entity ) == 1) {
-                  $parm->input['users_id_recipient'] = $parm->input['_users_id_requester'];
-                  $parm->input['_users_id_requester'] = $users_id;
-                  $parm->input['entities_id'] = current( $entity );
-               }
-            }
-         }
-
-
          // Analyze emails to establish conversation
          $references = [];
-         $is_local_mailgate = false;
          if (isset($mailgate)) {
             // mailgate has been open by web page call, then use it
             $local_mailgate = $mailgate;
@@ -223,7 +195,6 @@ class PluginMailAnalyzer {
                $parm->input = []; // empty array...
                return;
             }
-            $is_local_mailgate = true;
          }
 
          // try to get Thread-Index from email header
@@ -250,12 +221,6 @@ class PluginMailAnalyzer {
             // as Ticket creation is cancelled, then email is not deleted from mailbox
             // then we need to set deletion flag to true to this email from mailbox folder
             $local_mailgate->deleteMails($uid, MailCollector::REFUSED_FOLDER); // NOK Folder
-
-            // // close mailgate only if localy open
-            //if ($is_local_mailgate) {
-            //   // close session and delete emails marked for deletion during this session only!
-            //   unset($local_mailgate);
-            //}
 
             return;
          }
@@ -329,12 +294,6 @@ class PluginMailAnalyzer {
                   // then we need to set deletion flag to true to this email from mailbox folder
                   $local_mailgate->deleteMails($uid, MailCollector::ACCEPTED_FOLDER); // OK folder
 
-                  //// close mailgate only if localy open
-                  //if ($is_local_mailgate) {
-                  //   // close session and delete emails marked for deletion during this session only!
-                  //   unset($local_mailgate);
-                  //}
-
                   return;
                } else {
                   // ticket creation, but linked to the closed one...
@@ -368,9 +327,6 @@ class PluginMailAnalyzer {
       if (isset($parm->input['_mailgate'])) {
          // this ticket have been created via email receiver.
          // update the ticket ID for the message_id only for newly created tickets (ticket_id == 0)
-
-         // add watchers if ##CC
-         self::addWatchers($parm, $parm->fields['content']);
 
          $messages_id[] = $parm->input['_message']->messageid;
 
@@ -413,57 +369,57 @@ class PluginMailAnalyzer {
    }
 
 
-   /**
-    * Summary of addWatchers
-    * @param Ticket $parm    a Ticket
-    * @param string $content content that will be analyzed
-    * @return void
-    */
-   public static function addWatchers($parm, $content) {
-      // to be sure
-      if ($parm->getType() == 'Ticket') {
-         $content = str_replace(['\n', '\r\n'], "\n", $content);
-         $content = self::getTextFromHtml($content);
-         $ptnUserFullName = '/##CC\s*:\s*(["\']?(?\'last\'[\w.\-\\\\\' ]+)[, ]\s*(?\'first\'[\w+.\-\\\\\' ]+))?.*?(?\'email\'[\w_.+\-]+@[\w\-]+\.[\w\-.]+)?\W*$/imu';
-         if (preg_match_all($ptnUserFullName, $content, $matches, PREG_PATTERN_ORDER) > 0) {
-            // we found at least one ##CC matching user name convention: "Name, Firstname"
-            for ($i=0; $i<count($matches[1]); $i++) {
-               // then try to get its user id from DB
-               //$locUser = self::getFromDBbyCompleteName( trim($matches[1][$i]).' '.trim($matches[2][$i]));
-               $locUser = self::getFromDBbyCompleteName( trim($matches['last'][$i]).' '.trim($matches['first'][$i]));
-               if ($locUser) {
-                  // add user in watcher list
-                  if (!$parm->isUser( CommonITILActor::OBSERVER, $locUser->getID())) {
-                     // then we need to add this user as it is not yet in the observer list
-                     $locTicketUser = new Ticket_User;
-                     $locTicketUser->add( [ 'tickets_id' => $parm->getId(), 'users_id' => $locUser->getID(), 'type' => CommonITILActor::OBSERVER, 'use_notification' => 1] );
-                     $parm->getFromDB($parm->getId());
-                  }
-               }
-            }
-         }
+   ///**
+   // * Summary of addWatchers
+   // * @param Ticket $parm    a Ticket
+   // * @param string $content content that will be analyzed
+   // * @return void
+   // */
+   //public static function addWatchers($parm, $content) {
+   //   // to be sure
+   //   if ($parm->getType() == 'Ticket') {
+   //      $content = str_replace(['\n', '\r\n'], "\n", $content);
+   //      $content = self::getTextFromHtml($content);
+   //      $ptnUserFullName = '/##CC\s*:\s*(["\']?(?\'last\'[\w.\-\\\\\' ]+)[, ]\s*(?\'first\'[\w+.\-\\\\\' ]+))?.*?(?\'email\'[\w_.+\-]+@[\w\-]+\.[\w\-.]+)?\W*$/imu';
+   //      if (preg_match_all($ptnUserFullName, $content, $matches, PREG_PATTERN_ORDER) > 0) {
+   //         // we found at least one ##CC matching user name convention: "Name, Firstname"
+   //         for ($i=0; $i<count($matches[1]); $i++) {
+   //            // then try to get its user id from DB
+   //            //$locUser = self::getFromDBbyCompleteName( trim($matches[1][$i]).' '.trim($matches[2][$i]));
+   //            $locUser = self::getFromDBbyCompleteName( trim($matches['last'][$i]).' '.trim($matches['first'][$i]));
+   //            if ($locUser) {
+   //               // add user in watcher list
+   //               if (!$parm->isUser( CommonITILActor::OBSERVER, $locUser->getID())) {
+   //                  // then we need to add this user as it is not yet in the observer list
+   //                  $locTicketUser = new Ticket_User;
+   //                  $locTicketUser->add( [ 'tickets_id' => $parm->getId(), 'users_id' => $locUser->getID(), 'type' => CommonITILActor::OBSERVER, 'use_notification' => 1] );
+   //                  $parm->getFromDB($parm->getId());
+   //               }
+   //            }
+   //         }
+   //      }
 
-         $locGroup = new Group;
-         $ptnGroupName = "/##CC\s*:\s*([_a-z0-9-\\\\* ]+)/i";
-         if (preg_match_all($ptnGroupName, $content, $matches, PREG_PATTERN_ORDER) > 0) {
-            // we found at least one ##CC matching group name convention:
-            for ($i=0; $i<count($matches[1]); $i++) {
-               // then try to get its group id from DB
-               //if ($locGroup->getFromDBWithName( trim($matches[1][$i]))) {
-               if ($locGroup->getFromDBByCrit( ['name' => trim($matches[1][$i])])) {
-                  // add group in watcher list
-                  if (!$parm->isGroup( CommonITILActor::OBSERVER, $locGroup->getID())) {
-                     // then we need to add this group as it is not yet in the observer list
-                     $locGroup_Ticket = new Group_Ticket;
-                     $locGroup_Ticket->add( [ 'tickets_id' => $parm->getId(), 'groups_id' => $locGroup->getID(), 'type' => CommonITILActor::OBSERVER] );
-                     $parm->getFromDB($parm->getId());
-                  }
-               }
-            }
-         }
+   //      $locGroup = new Group;
+   //      $ptnGroupName = "/##CC\s*:\s*([_a-z0-9-\\\\* ]+)/i";
+   //      if (preg_match_all($ptnGroupName, $content, $matches, PREG_PATTERN_ORDER) > 0) {
+   //         // we found at least one ##CC matching group name convention:
+   //         for ($i=0; $i<count($matches[1]); $i++) {
+   //            // then try to get its group id from DB
+   //            //if ($locGroup->getFromDBWithName( trim($matches[1][$i]))) {
+   //            if ($locGroup->getFromDBByCrit( ['name' => trim($matches[1][$i])])) {
+   //               // add group in watcher list
+   //               if (!$parm->isGroup( CommonITILActor::OBSERVER, $locGroup->getID())) {
+   //                  // then we need to add this group as it is not yet in the observer list
+   //                  $locGroup_Ticket = new Group_Ticket;
+   //                  $locGroup_Ticket->add( [ 'tickets_id' => $parm->getId(), 'groups_id' => $locGroup->getID(), 'type' => CommonITILActor::OBSERVER] );
+   //                  $parm->getFromDB($parm->getId());
+   //               }
+   //            }
+   //         }
+   //      }
 
-      }
-   }
+   //   }
+   //}
 
 
    /**
