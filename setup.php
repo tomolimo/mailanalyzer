58e091ed381bd93eb9d12d00da3ef623a81fed82
tomolimo <olivier.moron@araymond.com>
2021-02-05 12:34:33 +0100

    Fixed returned value in $parm->input that must be false and not [] set version to 1.6.5

--- setup.php
+++ setup.php
@@ -1,30 +1,30 @@
 <?php
 
-define ("PLUGIN_MAILANALYSER_VERSION", "1.6.4");
+define ("PLUGIN_MAILANALYSER_VERSION", "1.6.5");
 
 // Init the hooks of the plugins -Needed
 function plugin_init_mailanalyzer() {
 
-    global $PLUGIN_HOOKS;
+   global $PLUGIN_HOOKS;
 
-    Plugin::registerClass('PluginMailAnalyzer', ['classname' => 'PluginMailAnalyzer']);
+   Plugin::registerClass('PluginMailAnalyzer', ['classname' => 'PluginMailAnalyzer']);
 
-    $PLUGIN_HOOKS['csrf_compliant']['mailanalyzer'] = true;
+   $PLUGIN_HOOKS['csrf_compliant']['mailanalyzer'] = true;
 
-    $PLUGIN_HOOKS['pre_item_add']['mailanalyzer'] = [
-           'Ticket' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer'],
-           'ITILFollowup' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer_followup']
-       ];
+   $PLUGIN_HOOKS['pre_item_add']['mailanalyzer'] = [
+      'Ticket'       => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer'],
+      'ITILFollowup' => ['PluginMailAnalyzer', 'plugin_pre_item_add_mailanalyzer_followup']
+      ];
 
-    $PLUGIN_HOOKS['item_add']['mailanalyzer'] = [
-           'Ticket' => ['PluginMailAnalyzer', 'plugin_item_add_mailanalyzer']
-       ];
+   $PLUGIN_HOOKS['item_add']['mailanalyzer'] = [
+      'Ticket' => ['PluginMailAnalyzer', 'plugin_item_add_mailanalyzer']
+      ];
 
 }
 
 // Get the name and the version of the plugin - Needed
 function plugin_version_mailanalyzer() {
-    global $LANG;
+   global $LANG;
 
     return  ['name'           => 'Mail Analyzer',
              'version'        => PLUGIN_MAILANALYSER_VERSION,
@@ -37,14 +37,14 @@ function plugin_version_mailanalyzer() {
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_mailanalyzer_check_prerequisites() {
    if (version_compare(GLPI_VERSION, '9.4', 'lt')) {
-       echo "This plugin requires GLPI >= 9.4";
-       return false;
+      echo "This plugin requires GLPI >= 9.4";
+      return false;
    } else {
-       return true;
+      return true;
    }
 }
 
 function plugin_mailanalyzer_check_config() {
-    return true;
+   return true;
 }
 
